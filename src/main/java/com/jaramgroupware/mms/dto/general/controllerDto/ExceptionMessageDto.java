a6900cca0230e2e5888b7bfeacb4ee33dc567185
Hyeon Hee <101989268+heegane@users.noreply.github.com>
2023-06-28 15:33:08 +0900

    [FEAT] 에러 메시지 통일

--- src/main/java/com/jaramgroupware/mms/dto/general/controllerDto/ExceptionMessageDto.java
+++ src/main/java/com/jaramgroupware/mms/dto/general/controllerDto/ExceptionMessageDto.java
@@ -3,14 +3,18 @@ package com.jaramgroupware.mms.dto.general.controllerDto;
 import lombok.*;
 import org.springframework.http.HttpStatus;
 
+import java.time.LocalDateTime;
+
 @ToString
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ExceptionMessageDto {
+    private String timestamp;
     private HttpStatus status;
-    private Integer type;
-    private String title;
-    private String detail;
+    private String error;
+    private String code;
+    private String message;
+    private String path;
 }
