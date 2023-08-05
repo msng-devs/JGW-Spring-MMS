7ccc141d4ce9c8b5680c87be661791340a0d54fa
Junser Hwang <hzser123@gmail.com>
2023-08-05 20:06:24 +0900

    [FEAT] Role Service Layer 테스트 추가

--- src/main/java/com/jaramgroupware/mms/service/RoleService.java
+++ src/main/java/com/jaramgroupware/mms/service/RoleService.java
@@ -12,34 +12,20 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 import static com.jaramgroupware.mms.utils.exception.service.ServiceErrorCode.*;
-/**
- * 권한 레벨에 관한 비즈니스 로직이 들어있는 클래스
- * @since 2023-03-07
- * @author 황준서(37기) hzser123@gmail.com
- * @author 이현희(38기) heeit13145@gmail.com
- */
+
 @RequiredArgsConstructor
 @Service
 public class RoleService {
 
     private final RoleRepository roleRepository;
 
-    /**
-     * 단일 권한 레벨을 조회하는 함수
-     * @param id 조회할 Role(Object)의 ID
-     * @return 조회된 Role(Object)의 정보를 담은 dto, 해당 데이터가 없을 시 INVALID_ROLE_ID 예외 처리
-     */
     @Transactional(readOnly = true)
     public RoleResponseDto findById(Long id){
-        Role targetRole = roleRepository.findById(id)
+        var targetRole = roleRepository.findById(id)
                 .orElseThrow(() -> new ServiceException(NOT_FOUND,"해당 Role을 찾을 수 없습니다."));
         return new RoleResponseDto(targetRole);
     }
 
-    /**
-     * 모든 권한 레벨을 조회하는 함수
-     * @return 모든 Role(Object)의 정보를 담은 dto(List type), 해당 데이터가 없을 시 EMPTY_ROLE 예외 처리
-     */
     @Transactional(readOnly = true)
     public List<RoleResponseDto> findAll(){
         return roleRepository.findAllByOrderByIdDesc()
--- src/test/java/com/jaramgroupware/mms/service/RoleServiceTest.java
+++ src/test/java/com/jaramgroupware/mms/service/RoleServiceTest.java
@@ -1,20 +1,63 @@
 package com.jaramgroupware.mms.service;
 
+import com.jaramgroupware.mms.domain.role.Role;
+import com.jaramgroupware.mms.domain.role.RoleRepository;
+import com.jaramgroupware.mms.dto.role.RoleResponseDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @ExtendWith(MockitoExtension.class)
 class RoleServiceTest {
 
+    @InjectMocks
+    private RoleService roleService;
+
+    @Mock
+    private RoleRepository roleRepository;
+
     @Test
     void findById() {
+        //given
+        doReturn(Optional.of(new Role(1L,"ROLE_USER"))).when(roleRepository).findById(1L);
+        //when
+        var result = roleService.findById(1L);
+        //then
+        assertEquals(result.getId(),1L);
+        assertEquals(result.getName(),"ROLE_USER");
+        verify(roleRepository).findById(1L);
+
     }
 
     @Test
     void findAll() {
+        //given
+        var roles = List.of(
+                new Role(1L,"ROLE_USER"),
+                new Role(2L,"ROLE_ADMIN")
+        );
+        doReturn(roles).when(roleRepository).findAllByOrderByIdDesc();
+
+        var excepted = List.of(
+                new RoleResponseDto(1L,"ROLE_USER"),
+                new RoleResponseDto(2L,"ROLE_ADMIN")
+        );
+        //when
+        var result = roleService.findAll();
+
+        //then
+        assertEquals(excepted,result);
+        verify(roleRepository).findAllByOrderByIdDesc();
     }
 }
