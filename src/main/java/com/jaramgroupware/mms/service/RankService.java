2d96d858d1bc41ea938d6c1fd63f46f00c0b53da
Junser Hwang <hzser123@gmail.com>
2023-08-05 14:37:34 +0900

    [FEAT] Rank Service 유닛테스트 추가

--- src/main/java/com/jaramgroupware/mms/service/RankService.java
+++ src/main/java/com/jaramgroupware/mms/service/RankService.java
@@ -14,34 +14,20 @@ import java.util.stream.Collectors;
 
 import static com.jaramgroupware.mms.utils.exception.service.ServiceErrorCode.NOT_FOUND;
 
-/**
- * 회원 등급에 관한 비즈니스 로직이 들어있는 클래스
- * @since 2023-03-07
- * @author 황준서(37기) hzser123@gmail.com
- * @author 이현희(38기) heeit13145@gmail.com
- */
+
 @RequiredArgsConstructor
 @Service
 public class RankService {
 
     private final RankRepository rankRepository;
 
-    /**
-     * 단일 회원 등급을 조회하는 함수
-     * @param id 조회할 Rank(Object)의 ID
-     * @return 조회된 Rank(Object)의 정보를 담은 dto
-     * @throws ServiceException 해당 데이터가 없을 시 NOT_FOUND 에러 발생
-     */
     @Transactional(readOnly = true)
     public RankResponseDto findById(Long id){
-        Rank targetRank = rankRepository.findById(id).orElseThrow(() -> new ServiceException(NOT_FOUND,"해당 Rank를 찾을 수 없습니다."));
+        var targetRank = rankRepository.findById(id).orElseThrow(() -> new ServiceException(NOT_FOUND,"해당 Rank를 찾을 수 없습니다."));
         return new RankResponseDto(targetRank);
     }
 
-    /**
-     * 모든 회원 등급을 조회하는 함수
-     * @return 모든 Rank(Object)의 정보를 담은 dto
-     */
+
     @Transactional(readOnly = true)
     public List<RankResponseDto> findAll(){
         return rankRepository.findAllBy()
--- src/test/java/com/jaramgroupware/mms/service/RankServiceTest.java
+++ src/test/java/com/jaramgroupware/mms/service/RankServiceTest.java
@@ -1,20 +1,84 @@
 package com.jaramgroupware.mms.service;
 
+import com.jaramgroupware.mms.domain.rank.Rank;
+import com.jaramgroupware.mms.domain.rank.RankRepository;
+import com.jaramgroupware.mms.dto.rank.RankResponseDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @ExtendWith(MockitoExtension.class)
 class RankServiceTest {
 
+    @Mock
+    private RankRepository rankRepository;
+
+    @InjectMocks
+    private RankService rankService;
+
     @Test
     void findById() {
+        //given
+        var target = Rank.builder()
+                .id(1L)
+                .name("정회원")
+                .build();
+
+        doReturn(Optional.of(target)).when(rankRepository).findById(1L);
+
+        var except = RankResponseDto.builder()
+                .id(1L)
+                .name("정회원")
+                .build();
+        //when
+        var result = rankService.findById(1L);
+
+        //then
+        assertEquals(except, result);
+
     }
 
     @Test
     void findAll() {
+        //given
+        var target = Rank.builder()
+                .id(1L)
+                .name("정회원")
+                .build();
+        var target2 = Rank.builder()
+                .id(2L)
+                .name("OB")
+                .build();
+
+        var rankList = List.of(target, target2);
+
+        doReturn(rankList).when(rankRepository).findAllBy();
+
+        var except = List.of(
+                RankResponseDto.builder()
+                        .id(1L)
+                        .name("정회원")
+                        .build(),
+                RankResponseDto.builder()
+                        .id(2L)
+                        .name("OB")
+                        .build()
+        );
+        //when
+        var result = rankService.findAll();
+
+        //then
+        assertEquals(except, result);
+
     }
 }
