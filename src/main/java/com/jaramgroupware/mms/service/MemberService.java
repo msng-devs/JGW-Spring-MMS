66c6b09507e5c02d562a539037edf3e7ff374f7a
Junser Hwang <hzser123@gmail.com>
2023-08-22 20:55:44 +0900

    [FEAT] 스케줄러 동작후 메일 전송하도록 추가

--- src/main/java/com/jaramgroupware/mms/service/MemberService.java
+++ src/main/java/com/jaramgroupware/mms/service/MemberService.java
@@ -262,7 +262,13 @@ public class MemberService {
             log.error("[WithdrawalScheduler] delete member error : {} {} {}",targetMember.getId(),targetMember.getEmail(),e.getMessage());
         }
 
+    }
 
+    @Transactional(readOnly = true)
+    public List<String> findEmailsByRole(Long roleId){
+        var targetRole = roleRepository.findRoleById(roleId)
+                .orElseThrow(() -> new ServiceException(ServiceErrorCode.NOT_FOUND, "존재하지 않는 Role입니다."));
+        return memberRepository.findAllByRole(targetRole).stream().map(Member::getEmail).toList();
     }
 
 }
