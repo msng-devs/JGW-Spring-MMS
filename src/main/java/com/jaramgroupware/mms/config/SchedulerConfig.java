d0d43043c12348f2f24a080d96e9334f4802bba5
Junser Hwang <hzser123@gmail.com>
2023-08-23 00:15:18 +0900

    [FIX] 스케줄러 Config 클래스에 @Configuration 어노테이션이 누락되었던 문제 수정

--- src/main/java/com/jaramgroupware/mms/config/SchedulerConfig.java
+++ src/main/java/com/jaramgroupware/mms/config/SchedulerConfig.java
@@ -1,7 +1,8 @@
 package com.jaramgroupware.mms.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+@Configuration
 @EnableScheduling
 public class SchedulerConfig {
 }
